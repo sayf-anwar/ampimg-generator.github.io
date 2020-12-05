279938f7d9f7cc53457d509b2367a2ed414e6a72
Sayf <santriweb@gmail.com>
2020-12-05 21:59:52 +0700

    Update autocopy.js

--- assets/js/autocopy.js
+++ assets/js/autocopy.js
@@ -1,7 +1,5 @@
-$("#button_id").click(function(){
-    var $temp = $("<textarea></textarea>");
-    $("body").append($temp);
-    $temp.val($("#textarea_source").val()).select();     <-- #textarea_source: id of textarea source to be copied to the clipboard
-    document.execCommand("copy");
-    $temp.remove();
-})
+function copy() {
+  let textarea = document.getElementById("textarea");
+  textarea.select();
+  document.execCommand("copy");
+}
